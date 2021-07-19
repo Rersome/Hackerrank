fcf1f3a9cd5f55ecb2f749d4881c538b8f51064e
Rersome <44253017+Rersome@users.noreply.github.com>
2021-07-20 04:44:53 +1000

    meh bubble

--- src/Zadachki/bubble.java
+++ src/Zadachki/bubble.java
@@ -2,7 +2,7 @@ package Zadachki;
 
 public class bubble {
     public static void main(String[] args){
-        int[] array = new int[]{9, 4, 2, 3, 1, 5, 6, 7, 8, 0};
+        int[] array = {9, 4, 2, 3, 1, 5, 6, 7, 8, 0};
         for (int i = 0; i < array.length; i++){
             for(int j = 0; j < array.length - i - 1; j++){
                 if (array[j] > array[j + 1]){
