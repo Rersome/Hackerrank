9f6d4abb5d9f453fd88dfbf5d14ae9168b14a2ee
Rersome <44253017+Rersome@users.noreply.github.com>
2021-07-20 04:55:59 +1000

    rly meh bubble

--- src/Zadachki/bubble.java
+++ src/Zadachki/bubble.java
@@ -1,18 +1,25 @@
 package Zadachki;
 
+import java.util.Scanner;
+
 public class bubble {
     public static void main(String[] args){
-        int[] array = {9, 4, 2, 3, 1, 5, 6, 7, 8, 0};
-        for (int i = 0; i < array.length; i++){
-            for(int j = 0; j < array.length - i - 1; j++){
-                if (array[j] > array[j + 1]){
-                    int dot = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = dot;
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int[] array = new int[10];
+        for (int a = 0;  a < n; a++) {
+            array[a] = sc.nextInt();
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array.length - i - 1; j++) {
+                    if (array[j] > array[j + 1]) {
+                        int dot = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = dot;
+                    }
                 }
             }
         }
-
         for (int i = 0; i < array.length; i++){
             System.out.println(array[i]);
         }
